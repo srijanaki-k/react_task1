8434d41db44608a307212639d4c417e3638b025b
srijanaki_k <srijanaki.katabathina@gmail.com>
2023-11-07 18:37:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+//Word Count
+//Task1
+import React from "react";
+import {useState,useEffect} from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+function App(){
+    const [text, setText] = useState('');
+    const [wordCount,setWordCount] = useState(0)
+    const [charCount,setCharCount] = useState(0)
+    
+    const changeHandle=(e)=>{
+        setText(e.target.value);
+    }
+    useEffect(() => {
+        console.log("effect")
+        // array of words
+        const words = text.split(' ');
+    
+        // update word count
+        let wordCount = 0;
+        words.forEach((word) => {
+          if (word.trim() !== '') {
+            wordCount++;
+          }
+        });
+        setWordCount(wordCount);
+    
+        // update char count (including whitespaces)
+        setCharCount(text.length);
+      }, [text]);
+    
+   
+    return(
+        <>
+        <div className="neg">
+        <div className="zero">
+        <h2 className="para">Responsive Paragraph Word Counter</h2>
+        <div>
+            <textarea className="one" onChange={changeHandle} value={text}/>
+            <p> Word count: {wordCount}</p>
+            <p> Char count(including spaces): {charCount}</p>
+        </div>
+        </div>
+        </div>
+        
+          
+        </>
+    )
+    
+      
+    
 }
-
-export default App;
+export default App
