11e84297a8d34af1b52c195b45d196d1aa3cd07a
srijanaki_k <srijanaki.katabathina@gmail.com>
2023-11-07 16:27:28 +0530

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Wordcounter } from "./components/wordcounter";
+function App() {
+  return (
+    <div>
+      <Wordcounter />
+    </div>
+  );
+}
+
+export default App;
